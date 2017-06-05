28b7daf280ef057cf01dd984ccb46aeef5d8c9cc
박다정 <daaa08@naver.com>
2017-06-06 00:32:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/da08/camera/Const.java
@@ -0,0 +1,14 @@
+package com.example.da08.camera;
+
+/**
+ * Created by Da08 on 2017. 6. 6..
+ */
+
+public class Const {
+
+    // 카메라 액션 상수값
+    public class Camera {
+        public static final int REQ_CAMERA = 100;
+    }
+
+}
